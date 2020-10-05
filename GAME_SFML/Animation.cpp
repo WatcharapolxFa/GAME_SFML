41362a0cc0921a14196033218e23e95bf294b7f2
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-10-06 04:26:48 +0700

    30

--- GAME_SFML/Animation.cpp
+++ GAME_SFML/Animation.cpp
@@ -1,1 +1,56 @@
 #include "Animation.h"
+#include <math.h>
+Animation::Animation(sf::Texture* texture, sf::Vector2u imageCount, float switchTime)
+{
+	this->imageCount = imageCount;
+	this->switchTime = switchTime;
+	totalTime = 0.0f;
+	currentImage.x = 0;
+
+	uvRect.width = texture->getSize().x / float(imageCount.x);
+	uvRect.height = texture->getSize().y / float(imageCount.y);
+}
+
+Animation::~Animation()
+{
+
+}
+
+void Animation::Update(int row, float deltaTime, bool faceRight)
+{
+	currentImage.y = row;
+	totalTime += deltaTime;
+
+	if (totalTime >= switchTime)
+	{
+		totalTime -= switchTime;
+		currentImage.x++;
+
+		if (currentImage.x >= imageCount.x)
+		{
+			currentImage.x = 0;
+		}
+	}
+
+	uvRect.top = currentImage.y * uvRect.height;
+
+	if (faceRight)
+	{
+		uvRect.left = currentImage.x * uvRect.width;
+		uvRect.width = abs(uvRect.width);
+	}
+	else
+	{
+		uvRect.left = (currentImage.x + 1) * abs(uvRect.width);
+		uvRect.width = -abs(uvRect.width);
+	}
+
+	if (row == 0)
+	{
+		switchTime = 0.23f;
+	}
+	else
+	{
+		switchTime = 0.1f;
+	}
+}
