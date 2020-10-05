2eacc0a1c314dbc094f82c07d10e530364b03fba
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-10-05 16:24:29 +0700

    20

--- GAME_SFML/maincpp.cpp
+++ GAME_SFML/maincpp.cpp
@@ -1,19 +1,56 @@
 #include <SFML/Graphics.hpp>
+#include<iostream>
+#include<stdio.h>
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1080, 720), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	
+	sf::RenderWindow window(sf::VideoMode(1080, 720), "Watcharapol Yotade 63010870", sf::Style::Close | sf::Style::Resize);
+	sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+	 player.setFillColor(sf::Color::Yellow);
+	 player.setPosition(206.0f, 206.0f);
+	 sf::Texture playerTexture;
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		sf::Event evnt;
+		while (window.pollEvent(evnt))
 		{
-			if (event.type == sf::Event::Closed)
+			switch (evnt.type)
+			{
+			case sf::Event::Closed:
 				window.close();
+				break;
+			case sf::Event::Resized:
+				std::cout <<" New window width : "<< evnt.size.width << " New window height  : " << evnt.size.height << std :: endl ;
+				break;
+			case sf::Event::TextEntered:
+				if (evnt.text.unicode < 128)
+				{
+					char show = evnt.text.unicode;
+					std::cout << show << std::endl;  // Show input Keyboard
+				}
+			}
 		}
+		// Line 32-48 input and move 
+		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+		{
+			player.move(-0.1f, 0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+		{
+			player.move(0.1f, 0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+		{
+			player.move(0.0f, -0.1f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+		{
+			player.move(0.0f, 0.1f);
+		}
+		
 		window.clear();
-		window.draw(shape);
+		window.draw(player);
 		window.display();
 	}
 	return 0;
