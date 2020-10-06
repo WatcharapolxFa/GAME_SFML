907bcc19723ca94ffd7dc5d01c5e6eeae85393d7
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-10-06 14:23:27 +0700

    30

--- GAME_SFML/Player.h
+++ GAME_SFML/Player.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include"Animation.h"
 class Player
 {
+public : 
+	Player();
+	~Player();
+
+	void Update(float deltaTime);
+	void Draw(sf::RenderWindow window);
+private : 
+	sf::RectangleShape body;
+	Animation animation;
+	unsigned int row;
+	float speed;
+	bool faceRight;
 };
 
