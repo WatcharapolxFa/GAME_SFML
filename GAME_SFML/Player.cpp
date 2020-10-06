f1e141dc4a0d04a1a1bfa87041d9ea1ba1c02dac
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-10-06 14:48:46 +0700

    25

--- GAME_SFML/Player.cpp
+++ GAME_SFML/Player.cpp
@@ -1,1 +1,47 @@
 #include "Player.h"
+Player::Player(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed):
+	animation(texture,imageCount,switchTime)
+{
+	this->speed = speed;
+	row = 0;
+	faceRight = true;
+	body.setSize(sf::Vector2f(120.0f, 196.0f));
+	body.setPosition(10.0f, 500.0f);
+	body.setTexture(texture);
+}
+Player :: ~Player()
+{
+
+}
+void Player::Update(float deltaTime)
+{
+	sf::Vector2f movement(0.0f, 0.0f);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+		movement.x -= speed * deltaTime;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		movement.x += speed * deltaTime;
+	}
+	if (movement.x == 0.0f)
+	{
+		row = 0;
+	}
+	else
+	{
+		row = 1;
+		if (movement.x > 0.0f)
+			faceRight = true;
+		else
+			faceRight = false;
+	}
+	animation.Update(row, deltaTime, faceRight);
+	body.setTextureRect(animation.uvRect);
+	body.move(movement);
+}
+void Player::Draw(sf::RenderWindow window)
+{
+	window.draw(body);
+}
