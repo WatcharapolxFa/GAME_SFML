203f7de5d5125f4cafa811d1d0841bdbc688a929
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-10-07 18:11:37 +0700

    gogo

---
+++ GAME_SFML/Collision.cpp
@@ -0,0 +1,15 @@
+#include "Collision.h"
+
+Collision::Collision(sf::RectangleShape& body) :
+	body(body)
+{
+
+}
+Collision::~Collision()
+{
+
+}
+bool Collision::CheckCollision(Collision& other, float push)
+{
+
+}
