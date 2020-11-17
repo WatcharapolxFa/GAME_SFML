f5cae8b0b894e1496ec95248d29ff1df517751f6
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-11-17 11:43:11 +0700

    menu

--- GAME_SFML/menu.cpp
+++ GAME_SFML/menu.cpp
@@ -5,22 +5,22 @@ Menu::Menu(float width, float height)
 	{ }
 	menu[0].setFont(font);
 	//menu[0].setCharacterSize(70);
-	menu[0].setFillColor(sf::Color::Red);
+	menu[0].setFillColor(sf::Color::Magenta);
 	menu[0].setString("Play");
 	menu[0].setOrigin(menu[0].getLocalBounds().width / 2, menu[0].getLocalBounds().height / 2);
-	menu[0].setPosition(sf::Vector2f(width / 2, height / (Max_ITEM + 1) * 1));
+	menu[0].setPosition(sf::Vector2f(width / 6, height / (Max_ITEM + 1) * 1));
 
 	menu[1].setFont(font);
 	//menu[1].setFillColor(sf::Color::Red);
 	menu[1].setString("Leaderboard");
 	menu[0].setOrigin(menu[1].getLocalBounds().width / 2, menu[1].getLocalBounds().height / 2);
-	menu[1].setPosition(sf::Vector2f(width / 2, height / (Max_ITEM + 1) * 2));
+	menu[1].setPosition(sf::Vector2f(width / 5, height / (Max_ITEM + 1) * 2.1));
 
 	menu[2].setFont(font);
 	//menu[2].setFillColor(sf::Color::Red);
 	menu[2].setString("Exit");
 	menu[0].setOrigin(menu[2].getLocalBounds().width / 2, menu[2].getLocalBounds().height / 2);
-	menu[2].setPosition(sf::Vector2f(width / 2, height / (Max_ITEM + 1) * 3));
+	menu[2].setPosition(sf::Vector2f(width / 4, height / (Max_ITEM + 1) * 3.3));
 
 
 }
@@ -41,7 +41,7 @@ void Menu::MoveUp()
 	{
 		menu[selectedItem].setFillColor(sf::Color::White);
 			selectedItem--;
-		menu[selectedItem].setFillColor(sf::Color::Red);
+		menu[selectedItem].setFillColor(sf::Color::Green);
 	}
 
 }
@@ -51,7 +51,7 @@ void Menu::MoveDown()
 	{
 		menu[selectedItem].setFillColor(sf::Color::White);
 		selectedItem++;
-		menu[selectedItem].setFillColor(sf::Color::Red);
+		menu[selectedItem].setFillColor(sf::Color::Yellow);
 	}
 
 }
